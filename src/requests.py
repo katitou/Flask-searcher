bbce7daf85a637201396c25f21ba10bbf135119f
Kristina Koshel <kristina.koschel@mail.ru>
2024-10-31 10:01:55 +1000

    Initialize mariadb, logging to sql database

--- src/requests.py
+++ src/requests.py
@@ -1,3 +1,32 @@
-def log_request(req: 'flask_request', res: str) -> None:
-    with open('search.log', 'a') as file:
-        print(req.remote_addr, req.user_agent, req.form, res, file=file, sep='|')
+from flask import Request
+import mysql.connector
+
+def log_request(req: Request, res: str) -> None:
+    """Loggging web-request and returning results."""
+    db_config = {
+        'host': '127.0.0.1', 
+        'user': 'admin', 
+        'password': 'admin', 
+        'database': 'searchlogDB', 
+        'collation':'utf8mb4_general_ci'
+    }
+
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+
+    _SQL = """
+        insert into log 
+        (phrase, letters, ip, browser_string, results)
+        values
+        (%s, %s, %s, %s, %s)
+        """
+    
+    cursor.execute(_SQL, (req.form['phrase'],
+                          req.form['letters'], 
+                          req.remote_addr,
+                          str(req.user_agent),
+                          res))
+    
+    conn.commit()
+    cursor.close()
+    conn.close()
